71feed8264764b5f06aca219e2d515f688d0433a
robertohgmv <robertohgm@hotmail.com>
2018-05-05 03:15:45 -0300

    Tela de barra de progresso

---
+++ src/FileUi.UI/ProgressBarForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace FileUi.UI
+{
+    partial class ProgressBarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbProgress = new MetroFramework.Controls.MetroLabel();
+            this.progressBar = new MetroFramework.Controls.MetroProgressBar();
+            this.SuspendLayout();
+            // 
+            // lbProgress
+            // 
+            this.lbProgress.AutoSize = true;
+            this.lbProgress.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lbProgress.Location = new System.Drawing.Point(8, 75);
+            this.lbProgress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbProgress.MaximumSize = new System.Drawing.Size(644, 25);
+            this.lbProgress.Name = "lbProgress";
+            this.lbProgress.Size = new System.Drawing.Size(50, 25);
+            this.lbProgress.TabIndex = 14;
+            this.lbProgress.Text = "100%";
+            this.lbProgress.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbProgress.UseWaitCursor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(8, 35);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(644, 35);
+            this.progressBar.TabIndex = 13;
+            this.progressBar.UseWaitCursor = true;
+            // 
+            // ProgressBarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 114);
+            this.Controls.Add(this.lbProgress);
+            this.Controls.Add(this.progressBar);
+            this.DisplayHeader = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressBarForm";
+            this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal MetroFramework.Controls.MetroLabel lbProgress;
+        internal MetroFramework.Controls.MetroProgressBar progressBar;
+    }
+}
---
+++ src/FileUi.UI/ProgressBarForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace FileUi.UI
+{
+    public partial class ProgressBarForm : MetroFramework.Forms.MetroForm
+    {
+        public ProgressBarForm()
+        {
+            InitializeComponent();
+        }
+
+        public void PositionChanged()
+        {
+            Application.DoEvents();
+        }
+    }
+}
